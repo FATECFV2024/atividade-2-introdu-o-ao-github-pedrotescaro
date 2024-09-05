fa60a486df379bad343d9e45a9a665473d37c017
Pedro Antonio Silvestre Tescaro <133606054+pedrotescaro@users.noreply.github.com>
2024-09-04 22:40:58 -0300

    [feat] adição do arquivo java do Aluno Pedro Tescaro

---
+++ FazerVsCode/src/ra_2920482411010.java
@@ -0,0 +1,17 @@
+public class ra_2920482411010 {
+    public static void main(String[] args) {
+        String nomeCompleto = "Pedro Antonio Silvestre Tescaro";
+        String ra = "2920482411010";
+        int idade = 19;
+        String github = "@pedrotescaro";
+        String linkedin = "@pedrotescaro";
+        String cursoETurma = "Analíse e desenvolvimento de sistemas e Segundo semestre";
+
+        System.out.println("Nome Completo: " + nomeCompleto);
+        System.out.println("RA: " + ra);
+        System.out.println("Idade: " + idade);
+        System.out.println("GitHub: " + github);
+        System.out.println("LinkedIn: " + linkedin);
+        System.out.println("Curso e Turma: " + cursoETurma);
+    }
+}
